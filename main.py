d1dafe14c4539cbedbd97bc3ae3784ed32348a8b
Wyzbits <wyzbits@gmail.com>
2023-03-16 19:35:52 +0100

    added publer webdriver script

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from edit_video import *
 from upload_video_to_insta import *
 from upload_video_to_tiktok import *
 from upload_video_to_youtube import *
+from upload import upload_video
 import subprocess
 
 
 def main():
-
     subprocess.run(
         ['python3', '-c', 'import gather_videos; gather_videos.get_reddit_videos()'])
     subprocess.run(["bash", "../tieUp.sh"])
+    subprocess.run["python3", "-c", "import upload;" "upload.upload_video()"]
 
 
 if __name__ == "__main__":
