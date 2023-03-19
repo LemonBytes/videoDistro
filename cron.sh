51835268a4b9d4d0d806e98cfaec26add87917ca
Wyzbits <wyzbits@gmail.com>
2023-03-19 18:55:02 +0100

    local

---
+++ cron.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/python3  /home/wyzbits/videoDistro/fetchVideo/main.py
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from gather_videos import *
-from edit_video import *
-from upload_video_to_insta import *
-from upload_video_to_tiktok import *
-from upload_video_to_youtube import *
 from upload import upload_video
 import subprocess
 
@@ -10,7 +6,7 @@ import subprocess
 def main():
     subprocess.run(
         ['python3', '-c', 'import gather_videos; gather_videos.get_reddit_videos()'])
-    subprocess.run(["bash", "../tieUp.sh"])
+   # subprocess.run(["bash", "../tieUp.sh"])
     subprocess.run["python3", "-c", "import upload;" "upload.upload_video()"]
 
 
