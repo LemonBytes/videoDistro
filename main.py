b3dc7cf0773da36f7cee807e979a473b47df3ada
Wyzbits <wyzbits@gmail.com>
2023-04-01 14:40:14 +0200

    longer sleep time for publish

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
-from gather_videos import * 
-from upload import upload_video 
+from gather_videos import *
+from upload import upload_video
 import subprocess
 
 
 def main():
-    subprocess.run(
-        ['python3', '-c', 'import gather_videos; gather_videos.get_reddit_videos()'])
+    # subprocess.run(
+    #   ['python', '-c', 'import gather_videos; gather_videos.get_reddit_videos()'])
    # subprocess.run(["bash", "../tieUp.sh"])
-    subprocess.run(['python3', '-c', 'import upload; upload.upload_video()'])
+    subprocess.run(['python', '-c', 'import upload; upload.upload_video()'])
+
 
 if __name__ == "__main__":
     main()
