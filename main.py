758d7c59ac586ad75ced46c4b9b74e11a64cbe19
Wyzbits <wyzbits@gmail.com>
2024-04-29 17:07:18 +0200

    changed gitignore

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from src.video_factory import VideoFactory
 
 
 def main():
-    print("Hello World!")
-    # video_factory = VideoFactory(max_limit=1, inject=True)
-    # video_factory.start()
+    video_factory = VideoFactory(max_limit=1, inject=None)
+    video_factory.start()
 
 
 if __name__ == "__main__":
